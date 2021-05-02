a64251a6f19a7d9caf6502fc0259daecaf9ec6c4
irakoze-hortance <hortanceirakoze@gmail.com>
2021-05-02 12:18:31 -0700

    initial commit

---
+++ src/design/InputBorder.java
@@ -0,0 +1,20 @@
+package design;
+
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class InputBorder implements Border {
+    private int radius;
+    InputBorder(int radius) {
+        this.radius = radius;
+    }
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+    }
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+    }
+}
