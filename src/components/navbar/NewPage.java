824a0d98a3d8c2e6d4ae745a4310c9c8e6775e24
edine-noella <edinenoella@gmail.com>
2021-05-10 20:47:17 +0200

    add navbar

---
+++ src/components/navbar/NewPage.java
@@ -0,0 +1,11 @@
+package Components.navbar;
+
+import javax.swing.*;
+
+
+public class NewPage extends JPanel {
+    public NewPage(){
+//        this.setBounds(300, 70, 1620, 780);
+//        this.setBackground(Color.WHITE);
+    }
+}
