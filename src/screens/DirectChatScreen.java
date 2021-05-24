c4ea3e66a128bdd1def1698f21644fed2f341fcd
Divin Irakiza <irakizadivin@gmail.com>
2021-05-24 13:17:43 +0200

    Chat Screen initiation

--- src/screens/DirectChatScreen.java
+++ src/screens/DirectChatScreen.java
@@ -1,10 +1,21 @@
 package screens;
 
+import javax.swing.*;
 import java.awt.*;
 
-public class DirectChatScreen extends Frame {
+public class DirectChatScreen extends JFrame {
     DirectChatScreen() {
+        this.setTitle("Chat Screen");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000, 700);
+        this.setLayout(new java.awt.BorderLayout(10, 10));
+
+
+        this.setBackground(new Color(255, 255, 255));
+
         UI();
+
+        this.setVisible(true);
     }
 
     public void UI() {
