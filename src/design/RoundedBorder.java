6d813299000e8bd0eb902af852555713c5f7c278
didiermunezero <didiermunezer38@gmail.com>
2021-06-04 12:56:25 +0200

    feat: full update message

--- src/design/RoundedBorder.java
+++ src/design/RoundedBorder.java
@@ -1,2 +1,21 @@
-package design;public class RoundedBorder {
+package design;
+
+
+import javax.swing.border.Border;
+import java.awt.*;
+
+class RoundedBorder implements Border {
+    private int radius;
+    RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+    }
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+    }
 }
