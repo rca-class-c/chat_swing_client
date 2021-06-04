5c870ed612b4745e7f6370de1fc637d26967d6c5
chanelle740 <hirwa.chanelle@gmail.com>
2021-06-04 07:50:14 +0200

    fix: currentUser

--- src/components/EditMessage.java
+++ src/components/EditMessage.java
@@ -11,16 +11,15 @@ import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
-public class EditMessage
-{
+public class EditMessage {
 
-    public void displayMessage(String passed){
+    public void displayMessage(String passed) {
         JFrame message = new JFrame();
         message.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        message.setBounds(150,60,250,150);
+        message.setBounds(150, 60, 250, 150);
         JLabel string = new JLabel(passed);
         JButton btn = new JButton("Close");
-        btn.setBackground(new Color(53,32,88));
+        btn.setBackground(new Color(53, 32, 88));
         btn.setForeground(Color.white);
         btn.setBorder(BorderFactory.createCompoundBorder(
                 btn.getBorder(),
@@ -28,47 +27,47 @@ public class EditMessage
         btn.setFont(new Font("Montserrat", Font.PLAIN, 14));
 //        btn.setBounds(50,220,300,30);
 
-        btn.addActionListener(ActionListener->{
+        btn.addActionListener(ActionListener -> {
             message.dispose();
         });
-        string.setBounds(0,0,20,1);
-        JPanel p =new JPanel();
+        string.setBounds(0, 0, 20, 1);
+        JPanel p = new JPanel();
         p.setBorder(new EmptyBorder(10, 10, 10, 10));
         p.add(string);
         p.add(btn);
         message.add(p);
         message.setVisible(true);
     }
-    EditMessage(Messages message){
-        JFrame f= new JFrame();
+
+    EditMessage(Messages message) {
+        JFrame f = new JFrame();
         Border border = BorderFactory.createLineBorder(Color.BLACK);
         JLabel header = new JLabel("Edit Message");
-        header.setBounds(10,30,400,30);
-        JTextArea area=new JTextArea(message.getContent());
-        area.setBounds(10,60, 450,150);
-        area.setFont(new Font("Roboto",Font.PLAIN,20));
-        header.setFont(new Font("Roboto",Font.PLAIN,25));
+        header.setBounds(10, 30, 400, 30);
+        JTextArea area = new JTextArea(message.getContent());
+        area.setBounds(10, 60, 450, 150);
+        area.setFont(new Font("Roboto", Font.PLAIN, 20));
+        header.setFont(new Font("Roboto", Font.PLAIN, 25));
         area.setBorder(BorderFactory.createCompoundBorder(border,
                 BorderFactory.createEmptyBorder(10, 10, 10, 10)));
         JButton btn = new JButton("Update Message");
-        btn.setBackground(new Color(53,32,88));
+        btn.setBackground(new Color(53, 32, 88));
         btn.setForeground(Color.white);
         btn.setBorder(BorderFactory.createCompoundBorder(
                 btn.getBorder(),
                 BorderFactory.createEmptyBorder(7, 30, 7, 30)));
         btn.setFont(new Font("Montserrat", Font.PLAIN, 18));
-        btn.setBounds(50,220,300,30);
-        btn.addActionListener(ActionListener->{
+        btn.setBounds(50, 220, 300, 30);
+        btn.addActionListener(ActionListener -> {
             String name = area.getText();
-            MessageResponseDataFormat messageResponseDataFormat = new MessageResponseDataFormat(message.getSender(), message.getId(),name);
+            MessageResponseDataFormat messageResponseDataFormat = new MessageResponseDataFormat(message.getSender(), message.getId(), name);
             String key = "messages/edit";
-            Request request = new Request(messageResponseDataFormat,key);
+            Request request = new Request(messageResponseDataFormat, key);
             ObjectMapper objectMapper = new ObjectMapper();
             ResponseDataSuccessDecoder responseDataSuccessDecoder = new IndexSocket().execute(request);
-            if(responseDataSuccessDecoder.isSuccess()){
+            if (responseDataSuccessDecoder.isSuccess()) {
                 displayMessage("Message updated successfully!");
-            }
-            else {
+            } else {
                 displayMessage("Failed to update message");
             }
         });
@@ -77,7 +76,8 @@ public class EditMessage
         f.add(btn);
         f.add(header);
         f.add(area);
-        f.setSize(500,300);
+        f.setSize(500, 300);
         f.setLayout(null);
         f.setVisible(true);
     }
+}
